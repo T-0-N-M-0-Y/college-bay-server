1c3392f8634cf9930e30538376828ff0df1c45e4
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-07-24 17:59:06 +0600

    update

--- index.js
+++ index.js
@@ -7,26 +7,6 @@ const port = process.env.PORT | 5000
 app.use(cors());
 app.use(express.json())
 
-// const verifyAccessWithJwtToken = (req, res, next) => {
-
-//     const authorization = req.headers.authorization;
-
-//     if (!authorization) {
-//         return res.status(401).send({ error: true, message: 'unauthorized access' });
-//     }
-
-//     const accessToken = authorization.split(' ')[1];
-
-//     jwt.verify(accessToken, process.env.ACCESS_TOKEN, (err, decoded) => {
-//         if (err) {
-//             return res.status(401).send({ error: true, message: 'unauthorized access' });
-//         }
-//         req.decoded = decoded;
-//         next();
-//     })
-// }
-
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.ddbcqih.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -48,24 +28,7 @@ async function run() {
         const collegesCollection = client.db('collegeBayDB').collection('colleges');
         const imagesCollection = client.db('collegeBayDB').collection('galary');
         const reviewsCollection = client.db('collegeBayDB').collection('reviews');
-
-                // // Verify Admin Middleware 
-                // const checkAdminOrUser = async (req, res, next) => {
-                //     const email = req.decoded.email;
-                //     const query = { email: email }
-                //     const user = await usersCollection.findOne(query);
-                //     if (user?.role !== 'admin' && user?.role !== 'user') {
-                //         return res.status(403).send({ error: true, message: 'forbidden access!' })
-                //     }
-                //     next();
-                // }
-        
-                // // Create jwt token
-                // app.post('/jwt', (req, res) => {
-                //     const user = req.body;
-                //     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
-                //     res.send({ accessToken });
-                // })
+        const applicationCollection = client.db('collegeBayDB').collection('appliedColleges');
 
 
         // users Collection Works
@@ -92,6 +55,12 @@ async function run() {
         })
 
         // Reviews Collection 
+        app.post('/reviews', async (req, res) => {
+            const addedreviews = req.body;
+            const result = await reviewsCollection.insertOne(addedreviews);
+            res.send(result)
+        })
+
         app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find().toArray();
             res.send(result)
@@ -110,32 +79,19 @@ async function run() {
             res.send(result)
         })
 
-        // app.patch('/users/admin/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const filter = { _id: new ObjectId(id) };
-        //     const updateDoc = {
-        //         $set: {
-        //             role: 'admin'
-        //         }
-        //     }
-        //     const result = await usersCollection.updateOne(filter, updateDoc);
-        //     res.send(result);
-        // })
+        // Applied Colleges 
+        app.post('/appliedColleges', async (req, res) => {
+            const appliedColleges = req.body;
+            const result = await applicationCollection.insertOne(appliedColleges);
+            res.send(result)
+        })
 
-            //   // Verify admin by email 
-            //   app.get('/users/admin/:email', verifyAccessWithJwtToken, async (req, res) => {
-            //     const email = req.params.email;
-    
-            //     if (req.decoded.email !== email) {
-            //         return res.send({ admin: false })
-            //     }
-    
-            //     const query = { email: email };
-            //     const user = await usersCollection.findOne(query);
-            //     const result = { admin: user?.role === 'admin' }
-            //     res.send(result)
-            // })
-
+        app.get('/appliedColleges/email', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await applicationCollection.find(query).toArray();
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
